100e9dd4c8379fc46d740f5780b52c65a094407b
jia216 <jia216@lehigh.edu>
2014-08-27 21:03:07 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//Jin Nam Ahn
+//hw01
+//
+//  first compile the program
+//      java WelcomeClass.java
+//  run the program
+//      java WelcomeClass
+
+//  define a class
+public class WelcomeClass{
+    
+    // add main method
+    public static void main(String[] args) {
+        
+        // print the statement with box around it
+        System.out.println(" ----------- ");
+        System.out.println(" | WELCOME | ");
+        System.out.println(" ----------- ");
+        System.out.println("   ^  ^  ^  ^  ^  ^   ");
+        System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\  ");
+        System.out.println(" |-j--i--a--2--1--6-| ");
+        System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ /  ");
+        System.out.println("   v  v  v  v  v  v   ");
+        System.out.println("This past summer, I traveled to California and the Dominican Republic.");
+    }   
+    
+}
